aa55bd02f9c648a104b303a6d2fbcaed9c06a261
Amarjeet Sinha <54766242+amarjeetsinha@users.noreply.github.com>
2022-06-09 11:50:45 +0530

    Add files via upload

---
+++ faulty calculator.py
@@ -0,0 +1,27 @@
+print("Welcome To The Amazing World Of Mathematics")
+print("Enter Your First Number")
+num1 = int(input())
+print("Which Opertaion Do You Want")
+opt = input()
+print("Enter Your Second Number")
+num2 = int(input())
+if opt == "+":
+    if num1== 45 and num2== 3:
+        print("Your Answer Is","555")
+    else:
+        print("Your Answer Is",num1+num2)
+if opt == "-":
+    if num1== 34 and num2== 87:
+        print("Your Answer Is","233")
+    else:
+        print("Your Answer Is",num1-num2)
+if opt == "*":
+    if num1 == 56 and num2 == 45:
+         print("Your Answer Is", "6447")
+    else:
+         print("Your Answer Is", num1*num2)
+if opt == "/":
+    if num1 == 28 and num2 == 98:
+         print("Your Answer Is", "46")
+    else:
+         print("Your Answer Is", num1/num2)
